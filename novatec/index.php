c2800e48c9a171e6d15216d68c2002b7b52de17d
Luansantoskr <50642691+Luansantoskr@users.noreply.github.com>
2020-12-10 13:43:33 -0300

    novatec
    Portfólio novatec

---
+++ novatec/index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Novatec</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="landing is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header" class="alt">
+						<h1><a href="index.html">Novatec</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="index.html">Início</a></li>
+											<li><a href="servicos.php">Serviços</a></li>
+											<li><a href="luan.php">Equipe</a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Banner -->
+					<section id="banner">
+						<div class="inner">
+							<h2>Novatec</h2>
+							<p>Tecnologia sem limites<br />
+							<ul class="actions special">
+								<li><a href="#" class="button primary">Portfólio</a></li>
+							</ul>
+						</div>
+						<a href="#one" class="more scrolly">Veja mais</a>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style1 special">
+						<div class="inner">
+							<header class="major">
+								<h2>Produção de sistemas e sites<br /></h2>
+								<p>Tendo em vista que cada dia mais se torna necessário estar atualizado, a novatec vem 
+                                    com a solução para um problema que cerca as empresas de todo o mundo: <strong>ATRASO NA EVOLUÇÃO TECNOLÓGICA.</strong>
+                                </p>
+							</header>
+						
+						</div>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper alt style2">
+						<section class="spotlight">
+							<div class="image"><img src="images/midia.jpg" alt="" /></div><div class="content">
+								<h2>Crescimento das mídias digitais</h2>
+                                <p>Investir em mídias digitais proporciona crescimento anual de 18% a empreendedores <br>
+                               Pesquisa realizada por Altimer Group e Wetpaint, para a revista Business week.</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="images/crescimento.jpg" alt="" /></div><div class="content">
+                                <h2>A importância da estratégia nas mídias digitais para a sua empresa.</h2>
+								<p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+							</div>
+						</section>
+						<section class="spotlight">
+							<div class="image"><img src="images/ecommerce.png" alt="" /></div><div class="content">
+								<h2>Crescimento com a pandemia</h2>
+								<p></p>
+							</div>
+						</section>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style3 special">
+						<div class="inner">
+							<header class="major">
+								<h2>Com todos esses dados, vamos ao que interessa.</h2>
+								<p>Conheça um pouco os serviços ofertados pela novatec.</p>
+							</header>
+							<ul class="features">
+								<li class="icon fa-paper-plane">
+									<h3>Criação de sites</h3>
+									<p></p>
+								</li>
+								<li class="icon solid fa-laptop">
+									<h3>Criação de sistemas</h3>
+									<p></p>
+								</li>
+								<li class="icon solid fa-code">
+									<h3>Serviço 3</h3>
+									<p></p>
+								</li>
+								<li class="icon solid fa-headphones-alt">
+									<h3>Serviço 4</h3>
+									<p></p>
+								</li>
+								<li class="icon fa-heart">
+									<h3>Serviço 5</h3>
+									<p></p>
+								</li>
+								<li class="icon fa-flag">
+									<h3>Serviço 6</h3>
+									<p></p>
+								</li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- CTA -->
+					<section id="cta" class="wrapper style4">
+						<div class="inner">
+							<header>
+								<h2>Acompanhe o crescimento tecnológico para sair na frente</h2>
+							</header>
+							<ul class="actions stacked">
+								<li><a href="#" class="button fit primary">Conheça mais</a></li>
+								<li><a href="#" class="button fit">O criador</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="icons">
+							<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>&copy; Todos os direitos reservados</li><li>Design: <a href="">Novatec</a></li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ novatec/servicos.php
@@ -0,0 +1,80 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Serviços</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1><a href="index.html">Serviços</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="index.php">Início</a></li>
+											<li><a href="servicos.php">serviços</a></li>
+											<li><a href="luan.php">Equipe</a></li>
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Main -->
+					<article id="main">
+						<header>
+							<h2>Conheça a equipe novatec</h2>
+						</header>
+						<section class="wrapper style5">
+							<div class="inner">
+
+								<h3>Serviço 1</h3>
+								<p>Dados do serviço</p>
+
+								<hr />
+
+								<h3>Serviço 2</h3>
+								<p>Dados do serviço</p>
+
+							</div>
+						</section>
+					</article>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<ul class="icons">
+							<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+							<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+							<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+						</ul>
+						<ul class="copyright">
+							<li>&copy; Todos os direitos reservados</li><li>Design: <a href="#">Novatec</a></li>
+						</ul>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
